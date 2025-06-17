54cb463aa9ad64f40713afbd43db461582927fcf
Shaikh Islam <shaikhalvee@gmail.com>
2025-06-16 20:57:53 -0500

    tried matfile, doesn't work. will remove and save by frames

--- beamform/txbf_range_doppler_postprocess.m
+++ beamform/txbf_range_doppler_postprocess.m
@@ -5,7 +5,7 @@ close all; clc; clearvars;
 
 
 % ----------------- USER CONFIG ---------------------------------------
-adc_data_folder = 'D:\Documents\Drone_Data\data\txbf_cor_drn_9_1_9_52db';
+adc_data_folder = 'D:\Documents\Drone_Data\data\txbf_prk_drn_9_1_9';
 [~, testRootFolder, ~] = fileparts(adc_data_folder);
 output_folder =  ['./output/' testRootFolder];
 oldParamsFile = [output_folder filesep testRootFolder '_params.mat'];
@@ -23,6 +23,11 @@ params.D_RX = configFromAdcData.D_RX;
 params.calibrationInterp = configFromAdcData.calibrationInterp;
 params.phaseCalibOnly = configFromAdcData.phaseCalibOnly;
 
+numAdc = params.Samples_per_Chirp;
+numChirp = params.nchirp_loops;
+numRx = params.numRX;
+numAngle = params.NumAnglesToSweep;
+
 
 % paramsConfig = struct;
 % I shouldn't repopulate here
@@ -33,8 +38,6 @@ params.phaseCalibOnly = configFromAdcData.phaseCalibOnly;
 % Calibration (RX phase)
 load(calib_file, 'calibResult');
 BF_MIMO_ref = calibResult.RxMismatch;
-% paramsConfig.Slope_MHzperus_calib = params.Slope_MHzperus;
-% paramsConfig.TI_Cascade_RX_ID = paramsConfig.TI_Cascade_RX_ID; % (already set by parameter_gen)
 
 % --------------- LOAD FILES & PROCESS EACH FRAME ----------------------
 [fileIdx_unique] = getUniqueFileIdx(adc_data_folder);
@@ -44,6 +47,25 @@ all_range_axis = {};
 all_doppler_axis = {};
 all_range_angle_stich = {};
 all_to_plot = {};
+
+% get number of frames
+% total_frame = 0;
+% for i_file = 1:numel(fileIdx_unique)
+%     [fileNameStruct] = getBinFileNames_withIdx(adc_data_folder, fileIdx_unique{i_file});
+%     [numValidFrames, ~] = getValidNumFrames(fullfile(adc_data_folder, fileNameStruct.masterIdxFile));
+%     total_frame = total_frame + numValidFrames;
+% end
+
+% total_frame = 200;
+
+% --- Preallocate matfile arrays on disk ---
+% mfile = matfile(fullfile(output_folder, "rangeDopplerFFTmap.mat"), "Writable", true);
+% mfile.all_to_plot = complex(zeros(numAdc, numChirp, numRx, numAngle, total_frame, 'single'));
+% mfile.all_range_axis = zeros(numAdc, total_frame, 'single');
+% mfile.all_doppler_axis = zeros(numChirp, total_frame,'single');
+% mfile.all_range_angle_stich = complex(zeros(numAdc, numAngle, total_frame, 'single'));
+
+frameCounter = 1;
 
 for i_file = 1:numel(fileIdx_unique)
     [fileNameStruct] = getBinFileNames_withIdx(adc_data_folder, fileIdx_unique{i_file});
@@ -57,13 +79,24 @@ for i_file = 1:numel(fileIdx_unique)
         radar_data_txbf = radar_data_txbf(:,:,params.TI_Cascade_RX_ID,:);
 
         % --------------- RANGE-DOPPLER PROCESSING & PLOT ---------------
-        [RD_map, range_axis, doppler_axis, range_angle_stich] = calc_range_doppler_bmfrm(radar_data_txbf, params, BF_MIMO_ref);
+        [RD_map, range_axis, doppler_axis, ...
+            range_angle_stich, params] = calc_range_doppler_bmfrm(radar_data_txbf, params, BF_MIMO_ref);
 
         % Store for saving later
         % all_RD_map{end+1} = RD_map;
         all_range_axis{end+1} = range_axis;
         all_doppler_axis{end+1} = doppler_axis;
         all_range_angle_stich{end+1} = range_angle_stich;
+
+        % mfile.all_to_plot(:,:,:,:,frameCounter) = single(RD_map);
+        % mfile.all_range_axis(:,frameCounter) = single(range_axis(:));
+        % mfile.all_doppler_axis(:,frameCounter) = single(doppler_axis(:));
+        % if ~isempty(range_angle_stich)
+        %     mfile.all_range_angle_stich(:,:,frameCounter) = single(range_angle_stich);
+        % end
+
+        % fprintf('[INFO] processing frame no: %d (global index %d)\n', frameId, frameCounter);
+        % frameCounter = frameCounter + 1;
 
         % ------- Plot: select RX channel or average ---------------------
         % If RD_map is 3D (range x doppler x RX), take mean across Rx
@@ -77,18 +110,23 @@ for i_file = 1:numel(fileIdx_unique)
         % else
         %     to_plot = abs(RD_map);
         % end
-        to_plot = abs(RD_map);
+        to_plot = RD_map;
         % saving plot data
         all_to_plot{end+1} = to_plot;
+        fprintf('[INFO] processing frame no: %d\n', frameCounter);
+        frameCounter = frameCounter + 1;
+
+        % clear RD_map range_axis doppler_axis range_angle_stich radar_data_txbf
 
         % --------------- DISPLAY ---------------------------------
 
-        display_graph(params, to_plot, range_axis, doppler_axis, range_angle_stich, 10);
+        % display_graph(params, to_plot, range_axis, doppler_axis, range_angle_stich, 10);
     end
 end
 
-% Save all processed RD maps *after* the loop
-% saving only to_plot, RD_map is taking too much space
+params.total_frames = frameCounter-1;
+
+
 save(fullfile(output_folder, "rangeDopplerFFTmap.mat"), "all_to_plot", ...
     "all_range_axis", "all_doppler_axis", "all_range_angle_stich", '-v7.3');
 
